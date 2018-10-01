63ca609bcb6de7c34037a69200f54c68532056a2
Mikdhad <32779021+Mikdhad@users.noreply.github.com>
2018-10-01 20:58:50 +0100

    Ex6

---
+++ src/makes10.java
@@ -0,0 +1,13 @@
+
+public class makes10 {
+	public boolean makes10(int a, int b) {
+		  if(a==10 || b==10){
+		    return true;
+		  }
+		  else if(a+b==10){
+		    return true;
+		  }
+		  return false;
+		}
+
+}
