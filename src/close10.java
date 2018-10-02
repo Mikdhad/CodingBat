8514c6602829dbd2e0e7290a7b45b36792b9fa59
Mikdhad <32779021+Mikdhad@users.noreply.github.com>
2018-10-03 00:43:43 +0100

    Made solution for close10 more efficient

--- src/close10.java
+++ src/close10.java
@@ -12,13 +12,11 @@ public class close10 {
 	 */
 	
 	public int close10(int a, int b) {
-		  if(Math.abs(a-10)==Math.abs(b-10)){
-		    return 0;
-		  }
-		  if(Math.abs(a-10)<Math.abs(b-10)){
-		    return a;
-		  }
-		  return b;
+		if(Math.abs(a-10)==Math.abs(b-10)){
+			return 0;
 		}
+		return Math.abs(a-10)<Math.abs(b-10) ? a : b;
+	}
+
 
 }
