6353d44e58318a16f1ed2363231f1af46eb02a54
Mikdhad <32779021+Mikdhad@users.noreply.github.com>
2018-10-06 21:35:30 +0100

    Added solution for firstHalf

---
+++ src/String1/firstHalf.java
@@ -0,0 +1,18 @@
+package String1;
+
+public class firstHalf {
+	/*
+	 * Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+
+		firstHalf("WooHoo") → "Woo"
+		firstHalf("HelloThere") → "Hello"
+		firstHalf("abcdef") → "abc"
+		
+		https://codingbat.com/prob/p172267
+	 */
+	public String firstHalf(String str) {
+		  return str.substring(0,str.length()/2);
+	}
+
+}
