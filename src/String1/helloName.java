4ffc3f73672199928b3eb60cf04b184cceb35b2e
Mikdhad <32779021+Mikdhad@users.noreply.github.com>
2018-10-06 21:20:59 +0100

    Added solution for helloName

---
+++ src/String1/helloName.java
@@ -0,0 +1,17 @@
+package String1;
+
+public class helloName {
+	/*
+	 * Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+
+		helloName("Bob") → "Hello Bob!"
+		helloName("Alice") → "Hello Alice!"
+		helloName("X") → "Hello X!"
+		
+		https://codingbat.com/prob/p171896
+	 */
+	public String helloName(String name) {
+		  return "Hello " + name + "!";
+	}
+}
