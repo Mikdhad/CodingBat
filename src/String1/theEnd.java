bd9814e06a120c7eccd3eac50170c21c74b9fa9c
Mikdhad <32779021+Mikdhad@users.noreply.github.com>
2018-10-07 19:55:18 +0100

    Added solution for theEnd

---
+++ src/String1/theEnd.java
@@ -0,0 +1,17 @@
+package String1;
+
+public class theEnd {
+	/*
+	 * Given a string, return a string length 1 from its front, unless front is false, in which case return a string length 1 from its back. The string will be non-empty.
+
+
+		theEnd("Hello", true) → "H"
+		theEnd("Hello", false) → "o"
+		theEnd("oh", true) → "o"
+		
+		https://codingbat.com/prob/p162477
+	 */
+	public String theEnd(String str, boolean front) {
+		  return (front)?str.charAt(0)+"":str.charAt(str.length()-1)+"";
+	}
+}
