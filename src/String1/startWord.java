dfc7ee3bddbaf98a8c011298dfa5a35c2926e140
Mikdhad <32779021+Mikdhad@users.noreply.github.com>
2018-10-09 00:58:24 +0100

    Added solution for startWord

---
+++ src/String1/startWord.java
@@ -0,0 +1,21 @@
+package String1;
+
+public class startWord {
+	/*
+	 * Given a string and a second "word" string, we'll say that the word matches the string if it appears at the front of the string, except its first char does not need to match exactly. On a match, return the front of the string, or otherwise return the empty string. So, so with the string "hippo" the word "hi" returns "hi" and "xip" returns "hip". The word will be at least length 1.
+
+
+		startWord("hippo", "hi") → "hi"
+		startWord("hippo", "xip") → "hip"
+		startWord("hippo", "i") → "h"
+		
+		https://codingbat.com/prob/p141494
+	 */
+	public String startWord(String str, String word) {
+		  if(str.length()>=word.length() && (word.equals(str.substring(0,word.length()))||word.substring(1,word.length()).equals(str.substring(1,word.length())))){
+		    return str.substring(0,word.length());
+		  }
+		  return "";
+	}
+
+}
