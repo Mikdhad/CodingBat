f5137519bfe0e61fadba91594862fa46e9d5fc1a
Mikdhad <32779021+Mikdhad@users.noreply.github.com>
2018-10-10 21:02:46 +0100

    Added solution for maxEnd3

---
+++ src/Array1/maxEnd3.java
@@ -0,0 +1,18 @@
+package Array1;
+
+public class maxEnd3 {
+	/*
+	 * Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value. Return the changed array.
+
+
+		maxEnd3([1, 2, 3]) → [3, 3, 3]
+		maxEnd3([11, 5, 9]) → [11, 11, 11]
+		maxEnd3([2, 11, 3]) → [3, 3, 3]
+		
+		https://codingbat.com/prob/p146256
+	 */
+	public int[] maxEnd3(int[] nums) {
+		  return nums[0]>nums[2]?new int[]{nums[0],nums[0],nums[0]}:new int[]{nums[2],nums[2],nums[2]};
+	}
+
+}
