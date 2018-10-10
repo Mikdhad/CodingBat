5777ca988437a4adb42a1959f42ed331aab27c12
Mikdhad <32779021+Mikdhad@users.noreply.github.com>
2018-10-10 20:00:22 +0100

    Added solution for makePi

---
+++ src/Array1/makePi.java
@@ -0,0 +1,17 @@
+package Array1;
+
+public class makePi {
+	/*
+	 * Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+
+		makePi() → [3, 1, 4]
+		
+		https://codingbat.com/prob/p167011
+	 */
+	public int[] makePi() {
+		  return new int[]{3,1,4};
+	}
+
+
+}
