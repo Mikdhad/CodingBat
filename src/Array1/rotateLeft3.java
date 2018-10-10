8030c7a86794657ab38e351809022bc132b19711
Mikdhad <32779021+Mikdhad@users.noreply.github.com>
2018-10-10 20:38:56 +0100

    Added solution for rotateLeft3

---
+++ src/Array1/rotateLeft3.java
@@ -0,0 +1,18 @@
+package Array1;
+
+public class rotateLeft3 {
+	/*
+	 * Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+
+
+		rotateLeft3([1, 2, 3]) → [2, 3, 1]
+		rotateLeft3([5, 11, 9]) → [11, 9, 5]
+		rotateLeft3([7, 0, 0]) → [0, 0, 7]
+		
+		https://codingbat.com/prob/p185139
+	 */
+	public int[] rotateLeft3(int[] nums) {
+		  return new int[]{nums[1],nums[2],nums[0]};
+	}
+
+}
