ce6ff857803a5abc023b739bf43ba23fb949f6e2
Mikdhad <32779021+Mikdhad@users.noreply.github.com>
2018-10-12 00:16:21 +0100

    Added solution for frontPiece

---
+++ src/Array1/frontPiece.java
@@ -0,0 +1,19 @@
+package Array1;
+
+public class frontPiece {
+	/*
+	 * 
+		Given an int array of any length, return a new array of its first 2 elements. If the array is smaller than length 2, use whatever elements are present.
+		
+		
+		frontPiece([1, 2, 3]) → [1, 2]
+		frontPiece([1, 2]) → [1, 2]
+		frontPiece([1]) → [1]
+		
+		https://codingbat.com/prob/p142455
+	 */
+	public int[] frontPiece(int[] nums) {
+		  return nums.length<1?nums:nums.length<2?nums:new int[]{nums[0],nums[1]};
+	}
+
+}
