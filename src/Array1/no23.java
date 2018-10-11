ce3a2630a1758cd06e2d5f9ba93d1d130049cc1b
Mikdhad <32779021+Mikdhad@users.noreply.github.com>
2018-10-11 23:28:29 +0100

    Added solution for no23

---
+++ src/Array1/no23.java
@@ -0,0 +1,18 @@
+package Array1;
+
+public class no23 {
+	/*
+	 * Given an int array length 2, return true if it does not contain a 2 or 3.
+
+
+		no23([4, 5]) → true
+		no23([4, 2]) → false
+		no23([3, 5]) → false
+		
+		https://codingbat.com/prob/p175689
+	 */
+	public boolean no23(int[] nums) {
+		  return (nums[0]!=2&&nums[0]!=3&&nums[1]!=2&&nums[1]!=3);
+	}
+
+}
