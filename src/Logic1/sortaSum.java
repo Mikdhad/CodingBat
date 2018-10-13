91befc3ace96860a3bfe0fcf26a289de14abef04
Mikdhad <32779021+Mikdhad@users.noreply.github.com>
2018-10-13 23:52:22 +0100

    Added solution for sortaSum

---
+++ src/Logic1/sortaSum.java
@@ -0,0 +1,21 @@
+package Logic1;
+
+public class sortaSum {
+	/*
+	 * Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+
+
+		sortaSum(3, 4) → 7
+		sortaSum(9, 4) → 20
+		sortaSum(10, 11) → 21
+		
+		https://codingbat.com/prob/p183071
+	 */
+	public int sortaSum(int a, int b) {
+		  if(a+b>9&&a+b<20){
+		    return 20;
+		  }
+		  return a+b;
+	}
+
+}
