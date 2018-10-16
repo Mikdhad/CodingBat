2b46a3a5e2f72333ebe63c2bd54b6b89049c5caf
Mikdhad <32779021+Mikdhad@users.noreply.github.com>
2018-10-16 21:16:55 +0100

    Added solution for less20

---
+++ src/Logic1/less20.java
@@ -0,0 +1,17 @@
+package Logic1;
+
+public class less20 {
+	/*
+	 * Return true if the given non-negative number is 1 or 2 less than a multiple of 20. So for example 38 and 39 return true, but 40 returns false. See also: Introduction to Mod
+
+
+		less20(18) → true
+		less20(19) → true
+		less20(20) → false
+		
+		https://codingbat.com/prob/p133158
+	 */
+	public boolean less20(int n) {
+		  return (n+1)%20==0||(n+2)%20==0;
+	}
+}
