9f6e36856eac755ccbc4d18b4c23333449572cf5
Mikdhad <32779021+Mikdhad@users.noreply.github.com>
2018-10-17 22:22:59 +0100

    Added solution for fizzString

---
+++ src/Logic1/fizzString.java
@@ -0,0 +1,27 @@
+package Logic1;
+
+public class fizzString {
+	/*
+	 * Given a string str, if the string starts with "f" return "Fizz". If the string ends with "b" return "Buzz". If both the "f" and "b" conditions are true, return "FizzBuzz". In all other cases, return the string unchanged. (See also: FizzBuzz Code)
+
+
+		fizzString("fig") → "Fizz"
+		fizzString("dib") → "Buzz"
+		fizzString("fib") → "FizzBuzz"
+		
+		https://codingbat.com/prob/p137136
+	 */
+	public String fizzString(String str) {
+		  if(str.charAt(0)=='f'&&str.charAt(str.length()-1)=='b') {
+		    return "FizzBuzz";
+		  }
+		  else if (str.charAt(0) == 'f') {
+		    return "Fizz";
+		  }
+		  else if ( str.charAt(str.length() - 1) == 'b') {
+		    return "Buzz";
+		  }
+		return str;
+		}
+
+}
